11a743aafac544c9100c1dd99b29059faa7923b2
Oleksandr <superbowl@ukr.net>
2022-09-27 16:09:24 +0300

    Update Section.js

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 const Section = ({ title, children }) => {
   return (
     <div>
-      <h2>{title}</h2>;{children};
+      <h2>{title}</h2>
+      {children}
     </div>
   );
 };
